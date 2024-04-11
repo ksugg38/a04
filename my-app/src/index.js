f43899946d1df2b25a70bddbdfe9e4d8402b514c
Katie Sugg <97493239+ksugg38@users.noreply.github.com>
2024-04-11 19:06:48 -0400

    Quotes work

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function MakeMeme(){
+  const[meme, setMeme] = useState("");
+
+  useEffect(() => {
+
+    fetch("http://apimeme.com/meme?meme=Condescending-Wonka&top=Top%20Text&bottom=Bottom%20Text")
+    .then(response => response.json())
+    .then(json => {
+      console.log(json.data.url)
+      setMeme(json.data.url);
+      
+      });
+  }, []);
+
+  return <img src={meme}></img>
+   
+}
+
+
+function KanyeQuote(){
+  const[kanyeQuote, setKanyeQuote] = useState("");
+
+  useEffect(() =>{
+    fetch('https://api.kanye.rest')
+      .then(response => response.json())
+      .then(data => {
+        setKanyeQuote(data.quote);
+    }).catch(error => console.error(error));
+  }, []);
+  
+  return <p>{kanyeQuote}</p>
+}
+
+function RonQuote(){
+  const[ronQuote, setRonQuote] = useState("");
+
+  useEffect(() =>{
+    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+      .then(response => response.json())
+      .then(data => {
+        setRonQuote(data[0]);
+    }).catch(error => console.error(error));
+  }, []);
+
+    return <p>{ronQuote}</p>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <RonQuote />
+    <KanyeQuote/>
+    <MakeMeme />
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+//,{
+  // method: "GET",
+  // mode: "cors"}
